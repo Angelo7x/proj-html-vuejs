28273b36888a04350ebbd94fc4647da38b1d307d
Angelo7x <abennici96@gmail.com>
2021-10-13 22:40:53 +0200

    finito footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,183 @@
 <template>
   <footer>
-
+    <div class="footer-container">
+      <ul>
+        <li><h3>AVADA UNIVERSITY</h3>
+          <ul>
+            <li v-for="(avadaElm, index) in avada" :key="index"><span v-html="avadaElm.avadaIcon"></span>  {{avadaElm.avadaLink}}</li>
+          </ul>
+        </li>
+        <li><h3>RECENT POSTS</h3>
+          <ul>
+            <li v-for="(postsElm, index) in posts" :key="index">
+              <a :href="postsElm.url"><span v-html="postsElm.postsIcon"></span>  {{postsElm.postsLink}}</a>
+              <div>{{postsElm.date}}</div>
+            </li>
+          </ul>
+        </li>
+        <li><h3>COURSES</h3>
+          <ul>
+            <li v-for="(coursesElm, index) in courses" :key="index">
+              <a :href="coursesElm.url">{{coursesElm.coursesLink}}</a>
+            </li>
+          </ul>
+        </li>
+        <li><h3>RESOURCES</h3>
+          <ul>
+            <li v-for="(resourcesElm, index) in resources" :key="index">
+              <a :href="resourcesElm.url">{{resourcesElm.resourcesLink}}</a>
+            </li>
+          </ul>
+        </li>
+        <li><h3>UPCOMING EVENTS</h3>
+          <ul>
+            <li v-for="(eventsElm, index) in events" :key="index">
+              {{eventsElm.eventsLink}}
+              <div>View More...</div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </footer>
 </template>
 
 <script>
 export default {
     name: "Footer",
+    data() {
+      return {
+        avada: [
+          {
+            avadaIcon: '<i class="fas fa-home"></i>',
+            avadaLink: "Residential Location 9521 Broadsberry Avenue",
+          },
+          {
+            avadaIcon: '<i class="fas fa-phone"></i>',
+            avadaLink: "1.800.567.8910",
+          },
+          {
+            avadaIcon: '<i class="fas fa-envelope"></i>',
+            avadaLink: "info@your-domain.com",
+          },
+          {
+            avadaIcon: '<i class="far fa-clock"></i>',
+            avadaLink: "Monday - Friday 8:00 AM - 10:00 PM",
+          },
+        ],
+        posts: [
+          {
+            postsLink: "What do successful grads think you should study?",
+            url: "#",
+            date: "March 16, 2016",
+            postsIcon: '<i class="fas fa-angle-right"></i>',
+          },
+          {
+            postsLink: "Former student discusses success in the fashion industry",
+            url: "#",
+            date: "March 16, 2016",
+            postsIcon: '<i class="fas fa-angle-right"></i>',
+          },
+          {
+            postsLink: "How do you best prepare for university?",
+            url: "#",
+            date: "March 16, 2016",
+            postsIcon: '<i class="fas fa-angle-right"></i>',
+          },
+        ],
+        courses: [
+          {
+            coursesLink: "Arts & Humanities",
+            url: "#",
+            
+          },
+          {
+            coursesLink: "Business & Management",
+            url: "#",
+          },
+          {
+            coursesLink: "Engineering & Technology",
+            url: "#",
+          },
+          {
+            coursesLink: "Life Sciences & Medicine",
+            url: "#",
+          },
+          {
+            coursesLink: "Natural Sciences",
+            url: "#",
+          },
+        ],
+        resources: [
+          {
+            resourcesLink: "Campus Directory",
+            url: "#",
+          },
+          {
+            resourcesLink: "Academic Calendar",
+            url: "#",
+          },
+          {
+            resourcesLink: "Sport Clubs",
+            url: "#",
+          },
+          {
+            resourcesLink: "Graduate Programs",
+            url: "#",
+          },
+          {
+            resourcesLink: "Faculty & Staff",
+            url: "#",
+          },
+        ],
+        events: [
+          {
+            eventsLink: "Business Seminar",
+          },
+          {
+            eventsLink: "Politics Seminar",
+          },
+        ]
+      }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-
+footer {
+  margin-top: 300px;
+  .footer-container {
+    @import '../assets/style/variables';
+    width: $sizeFooterContainer;
+    // height: 200px;
+    margin: auto;
+    a {
+      text-decoration: none;
+      color: black;
+    }
+    a:hover {
+      opacity: 0.6;
+    }
+    ul {
+      display: flex;
+      justify-content: space-between;
+      list-style: none;
+      h3 {
+        color: #333c4e;
+        margin-bottom: 20px;
+      }
+      ul {
+        display: flex;
+        flex-direction: column;
+        div {
+          color: rgb(177, 177, 177);
+        }
+        li {
+          padding: 20px 0;
+          display: inline-block;
+        }
+      }
+    }
+  }
+}
 </style>
