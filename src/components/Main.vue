abade01d05b8bdf73e6bdd689d47858348223e72
Angelo7x <abennici96@gmail.com>
2021-10-14 02:17:57 +0200

    adds section hero

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,13 @@
 <template>
   <section>
-
+    <div class="first-section-hero">
+      <div class="hero-text">
+        <h2>Inspiring Tomorrows Leaders</h2>
+        <h4>The Future Belongs To those Who See Possibilities Today</h4>
+        <button class="btn orange-btn">FIND A COURSE</button>
+        <button class="btn blue-btn">APPLY NOW</button>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -11,5 +18,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+section {
+  margin-top: 130px;
+}
+.first-section-hero {
+  height: 550px;
+  background-image: url('../assets/img/home_main_slider_bg.jpg');
+  background-repeat: no-repeat;
+  background-position: center;
+  object-fit: cover;
+  position: relative;
+  .hero-text {
+    font-size: 40px;
+    color: #fff;
+    text-align: center;
+    margin-top: auto;
+  }
+}
 </style>
